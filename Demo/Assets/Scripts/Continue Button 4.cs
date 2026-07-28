ab926cf0d75a453b58b390725896d385b1aa3517
tiotabitha-source <tiotabitha@gmail.com>
2026-07-28 15:15:34 -0700

    more scenes

---
+++ Demo/Assets/Scripts/Continue Button 4.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueeeeButton : MonoBehaviour
+{
+
+    public void bottomsScene() {
+        SceneManager.LoadScene("Bottoms");
+    }
+
+    
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+}
