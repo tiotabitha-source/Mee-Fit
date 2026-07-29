d1f5e4b9d45080a212fb7125d2727054dccba664
tiotabitha-source <tiotabitha@gmail.com>
2026-07-29 11:47:52 -0700

    Update HairUIDriver.cs

--- Demo/Assets/Scripts/HairUIDriver.cs
+++ Demo/Assets/Scripts/HairUIDriver.cs
@@ -5,6 +5,11 @@ public class HairUIDriver : MonoBehaviour {
     public GameObject option1;
     public GameObject option3;
 
+    // void Start() {
+    //     if (Attributes.instance.skinTone == "Option 1") {
+            
+    //     }
+    }
     public void changeHair1() {
         Attributes.instance.ChangeHair("Option 1");
         option1.SetActive(true);
